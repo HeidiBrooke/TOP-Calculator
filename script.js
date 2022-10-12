cf467898c934a884927b1ea0ad097ab5411fae94
HeidiBrooke <hbrooke36@yahoo.com>
2022-10-12 15:06:47 -0700

    Add functions +, -, *, / and operate

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-console.log("hey hey hey");
+//Basic functions +, -, *, /
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+// console.log(add(10,2));
+// console.log(subtract(10,2));
+// console.log(multiply(10,2));
+// console.log(divide(10,2));
+
+//Operator
+function operate(operator, num1, num2){
+    let result = null;
+    switch (operator) {
+        case '+': 
+            result = add(num1,num2);
+            break;
+        case '-': 
+            result = subtract(num1,num2);
+            break;
+        case '*': 
+            result = multiply(num1,num2);
+            break;
+        case '/': 
+            result = divide(num1,num2);
+            break;
+    }
+    return result;
+}
+
+// console.log(operate('+', 10, 2));
+// console.log(operate('-', 10, 2));
+// console.log(operate('*', 10, 2));
+// console.log(operate('/', 10, 2));

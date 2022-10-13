cd2a0e4ca39f98fa1f3457b6b5f07d738fd0c9cb
HeidiBrooke <hbrooke36@yahoo.com>
2022-10-13 16:59:11 -0700

    Add display funtionality for clicked numbers

--- script.js
+++ script.js
@@ -44,3 +44,25 @@ function operate(operator, num1, num2){
 // console.log(operate('-', 10, 2));
 // console.log(operate('*', 10, 2));
 // console.log(operate('/', 10, 2));
+// const currentNumber;
+// const display;
+//display update function
+function displayUpdate(e){
+    let num = e.target.innerHTML;
+    console.log(num);
+    let display = document.getElementById('display');
+    console.log(display);
+    display.textContent = num;
+
+}
+
+function addListeners(){
+    const divs = document.querySelectorAll('.numbers'); 
+    console.log(divs);
+    divs.forEach(div => div.addEventListener('click', displayUpdate));
+}
+
+addListeners();
+// const divs = document.querySelectorAll('.number');
+// console.log(divs);
+// divs.forEach(div => div.addEventListener('click', displayUpdate));
